0d24289bfb75acc1c7dacaf2aaa36b41da081950
minseon33 <at36936900@gmail.com>
2023-04-28 16:23:38 +0900

    알고리즘 강의 3-6,4-1,4-2,4-3

---
+++ src/main/java/InflearnAlgorethm/twoPointersSlidingWindow/InflearnAlgorethm_7.java
@@ -0,0 +1,42 @@
+package InflearnAlgorethm.twoPointersSlidingWindow;
+
+import java.util.Scanner;
+
+
+/**
+ * 6. 최대길이 연속부분 수열
+ */
+public class InflearnAlgorethm_7 {
+    public int solution(int n, int k, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) {
+                cnt++;
+            }
+            while (cnt > k) {
+                if (arr[lt] == 0) {
+                    cnt--;
+                }
+                lt++;
+
+            }
+            answer = Math.max(answer, rt - lt + 1);
+        }
+        return answer;
+
+    }
+
+
+    public static void main(String[] args) {
+        InflearnAlgorethm_7 T = new InflearnAlgorethm_7();
+        //1. 스캐너로 첫번째 값 받아오기
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(n, k, a));
+    }
+}
