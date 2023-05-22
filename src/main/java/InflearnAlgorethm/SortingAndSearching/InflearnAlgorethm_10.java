5268a8b50ec4898649df7870bdec72b32057625a
minseon33 <at36936900@gmail.com>
2023-05-22 12:14:19 +0900

    알고리즘 강의,(결정알고리즘)뮤직비디오, 마굿간 정하기

---
+++ src/main/java/InflearnAlgorethm/SortingAndSearching/InflearnAlgorethm_10.java
@@ -0,0 +1,62 @@
+package InflearnAlgorethm.SortingAndSearching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+/**
+ * 10. 마굿간 정하기
+ */
+public class InflearnAlgorethm_10 {
+    public int count(int[] arr,int dist){
+        int cnt = 1;
+        int ep=arr[0];
+
+        for(int i=1; i <arr.length; i++) {
+            if (arr[i] - ep >= dist) {
+                cnt++;
+                ep = arr[i];
+
+            }
+        }
+
+
+        return cnt;
+    }
+    public int solution(int n, int m ,int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt=1;
+        int rt=arr[n-1];
+
+        while (lt<=rt){
+            int mid = (lt+rt)/2;
+            if(count(arr,mid)>=m){
+                answer=mid;
+                lt=mid+1;
+            }else{
+                rt=mid-1;
+            }
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        InflearnAlgorethm_10 T = new InflearnAlgorethm_10();
+        //1. 스캐너로 첫번째 값 받아오기
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]= kb.nextInt();
+        }
+
+        System.out.println(T.solution(n,m,arr));
+
+    }
+
+
+}
